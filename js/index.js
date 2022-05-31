cbfa5c2d26c729dfdcdbac7e5b093d010b8272d0
Matth5050 <matthew.f.herbert@gmail.com>
2022-05-31 14:05:24 -0700

    created a temp calculator dquote> dquote> Co-authored-by: Winnie Wang <weijwang18@gmail.com> dquote> Co-authored-by: Mo Byrne <mosey.b.byrne@gmail.com>

--- js/index.js
+++ js/index.js
@@ -1,24 +1,33 @@
-function add(number1, number2) {
-  return number1 + number2;
+// function add(number1, number2) {
+//   return number1 + number2;
+// }
+
+// function subtract(number1, number2) {
+//   return number1 - number2;
+// }
+
+// function multiply(number1, number2) {
+//   return number1 * number2;
+// }
+
+// function remainder(number1, number2) {
+//   return number1 % number2;
+// }
+
+// const number1 = parseInt(prompt("enter a number:"));
+// const number2 = parseInt(prompt("Enter another number:"));
+// const addResult = add(number1, number2);
+// const subtractResult = subtract(number1, number2);
+// const multiplyResult = multiply(number1, number2);
+// const remainderResult = remainder(number1, number2);
+
+// alert(remainderResult);
+
+const userInput = prompt("What is the temperature in Celsius?");
+
+function convertToFa(temp) {
+  let conversion = (temp * 9) / 5 + 32;
+  alert("the temp is " + conversion + "°F");
 }
 
-function subtract(number1, number2) {
-  return number1 - number2;
-}
-
-function multiply(number1, number2) {
-  return number1 * number2;
-}
-
-function remainder(number1, number2) {
-  return number1 % number2;
-}
-
-const number1 = parseInt(prompt("enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-const addResult = add(number1, number2);
-const subtractResult = subtract(number1, number2);
-const multiplyResult = multiply(number1, number2);
-const remainderResult = remainder(number1, number2);
-
-alert(remainderResult);
+convertToFa(userInput);
