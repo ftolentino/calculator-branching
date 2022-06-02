28315f3339f45692b5f67948e0a5134c65cd63de
Matth5050 <matthew.f.herbert@gmail.com>
2022-06-02 10:08:21 -0700

    added funcitons and forms to allow for multiplication, subtraction, and reamainder
    Co-authored-by: Filerm Tolentino <filmer.tolentino@gmail.com>
    Co-authored-by: Winnie Wang <weijwang18@gmail.com>

--- js/index.js
+++ js/index.js
@@ -15,7 +15,9 @@ function multiply(number1, number2) {
 function remainder(number1, number2) {
   return number1 % number2;
 }
+
 //UI Logic
+
 $(document).ready(function() {
   $("form#add").submit(function(event) {
   event.preventDefault();
@@ -23,23 +25,44 @@ $(document).ready(function() {
   const number2 = parseInt($("#add2").val());
   const addResult = add(number1, number2);
   $("#output").text(addResult);
+  });
+  
+  $("form#subtract").submit(function(event) {
+  event.preventDefault();
+  const number1 = parseInt($("#sub1").val());
+  const number2 = parseInt($("#sub2").val());
+  const subtractResult = subtract(number1, number2);
+  $("#output").text(subtractResult);
 });
-})
+
+$("form#multiply").submit(function(event) {
+  event.preventDefault();
+  const number1 = parseInt($("#mul1").val());
+  const number2 = parseInt($("#mul2").val());
+  const multiplyResult = multiply(number1, number2);
+  $("#output").text(multiplyResult);
+});
+
+$("form#remainder").submit(function(event) {
+  event.preventDefault();
+  const number1 = parseInt($("#remain1").val());
+  const number2 = parseInt($("#remain2").val());
+  const remainderResult = remainder(number1, number2);
+  $("#output").text(remainderResult);
+});
+
+
+
+});
+
+
+
+
+
 
 // const subtractResult = subtract(number1, number2);
 // const multiplyResult = multiply(number1, number2);
 // const remainderResult = remainder(number1, number2);
-
-
-
-
-
-
-
-
-
-
-
 
 // const userInput = prompt("What is the temperature in Celsius?");
 
