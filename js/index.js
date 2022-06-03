8a6a7179032095f1fdb15ca801b0c77d4f060cd6
ftolentino <filmer.tolentino@gmail.com>
2022-06-02 21:28:54 -0700

    ADD branching logic to refactor code
    Co-authored-by: Ash Urban <ashe@goldentongue.com>

--- js/index.js
+++ js/index.js
@@ -12,47 +12,31 @@ function multiply(number1, number2) {
   return number1 * number2;
 }
 
-function remainder(number1, number2) {
-  return number1 % number2;
+function divide(number1, number2) {
+  return number1 / number2;
 }
 
 //UI Logic
 
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
+  $("form#calculator").submit(function(event) {
   event.preventDefault();
-  const number1 = parseInt($("#add1").val());
-  const number2 = parseInt($("#add2").val());
-  const addResult = add(number1, number2);
-  $("#output").text(addResult);
-  });
+  const number1 = parseInt($("#input1").val());
+  const number2 = parseInt($("#input2").val());
+  const operator = $("input:radio[name=operator]:checked").val();
+  let result;
+  if (operator === "add") {
+    result = add(number1, number2);
+  } else if (operator === "subtract") {
+    result = subtract(number1, number2);
+  } else if (operator === "multiply") {
+    result = multiply(number1, number2);
+  } else if (operator === "divide") {
+    result = divide(number1, number2);
+  }
+  $("#output").text(result);
   
-  $("form#subtract").submit(function(event) {
-  event.preventDefault();
-  const number1 = parseInt($("#sub1").val());
-  const number2 = parseInt($("#sub2").val());
-  const subtractResult = subtract(number1, number2);
-  $("#output").text(subtractResult);
-});
-
-$("form#multiply").submit(function(event) {
-  event.preventDefault();
-  const number1 = parseInt($("#mul1").val());
-  const number2 = parseInt($("#mul2").val());
-  const multiplyResult = multiply(number1, number2);
-  $("#output").text(multiplyResult);
-});
-
-$("form#remainder").submit(function(event) {
-  event.preventDefault();
-  const number1 = parseInt($("#remain1").val());
-  const number2 = parseInt($("#remain2").val());
-  const remainderResult = remainder(number1, number2);
-  $("#output").text(remainderResult);
-});
-
-
-
+  });
 });
 
 
@@ -60,24 +44,3 @@ $("form#remainder").submit(function(event) {
 
 
 
-// const subtractResult = subtract(number1, number2);
-// const multiplyResult = multiply(number1, number2);
-// const remainderResult = remainder(number1, number2);
-
-// const userInput = prompt("What is the temperature in Celsius?");
-
-// function convertToFa(temp) {
-//   let conversion = (temp * 9) / 5 + 32;
-//   alert("the temp is " + conversion + "°F");
-// }
-
-// convertToFa(userInput);
-
-// const userInput = prompt("Enter gallons");
-
-// function convertToLiters(gal) {
-//   let convertion = gal * 3.785412;
-//   alert(convertion + " liters");
-// }
-
-// convertToLiters(userInput);
